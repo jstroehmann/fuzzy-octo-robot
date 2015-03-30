dd9221f03021b51989e8f0c97bbbfa6fb4ffe85d
James Stroehmann <jaymz@jaymz.org>
2015-03-30 07:32:33 -0400

    .

--- scripts/build-website.sh
+++ scripts/build-website.sh
@@ -1,13 +1,13 @@
 mkdir -p ~/.aws/
 cat <<EOF >> ~/.aws/config
 [default]
-aws_access_key_id = AKIAJHFRCRDZI5KIBCMA
-aws_secret_access_key = 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
+aws_access_key_id = XXXXXXXXXXXXXXXXXXXXXXX
+aws_secret_access_key = XXXXXXXXXXXXXXXXXXXXXX
 region=us-east-1
 output=json
 EOF
 
-/opt/packer/packer build -machine-readable -var aws_access_key=AKIAJHFRCRDZI5KIBCMA -var aws_secret_key=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 packer/packer.json |tee packer.output
+/opt/packer/packer build -machine-readable -var aws_access_key=XXXXXXXXXXXXXXXXXXXXXXXX -var aws_secret_key=XXXXXXXXXXXXXXXXXXXXXXXX packer/packer.json |tee packer.output
 AMI_ID=$(awk -F: '/artifact,0,id/ {print $2}' packer.output)
 echo $AMI_ID
 aws s3 cp cftemplates/website.template s3://cf-templates-1bpde8xxropa1-us-east-1
