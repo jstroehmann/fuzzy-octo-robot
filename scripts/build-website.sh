ad825ed7d5610301b5aa8f2a2993c8c7355fbd2b
James Stroehmann <jaymz@jaymz.org>
2015-03-30 01:01:39 -0400

    updated the website build script

--- scripts/build-website.sh
+++ scripts/build-website.sh
@@ -1,7 +1,14 @@
-export aws_access_key='XXXXXXXXXXXXXXXXXXXXXXX'
-export aws_secret_key='XXXXXXXXXXXXXXXXXXXXXXX'
-/opt/packer/packer build -machine-readable packer/packer.json |tee packer.output
-AMI_ID=$(awk '/artifact,0,id/ {print $2}' packer.output)
+mkdir -p ~/.aws/
+cat <<EOF >> ~/.aws/config
+[default]
+aws_access_key_id = AKIAJHFRCRDZI5KIBCMA
+aws_secret_access_key = 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
+region=us-east-1
+output=json
+EOF
+
+/opt/packer/packer build -machine-readable -var aws_access_key=AKIAJHFRCRDZI5KIBCMA -var aws_secret_key=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 packer/packer.json |tee packer.output
+AMI_ID=$(awk -F: '/artifact,0,id/ {print $2}' packer.output)
 echo $AMI_ID
 aws s3 cp cftemplates/website.template s3://cf-templates-1bpde8xxropa1-us-east-1
-aws cloudformation create-stack --stack-name "website$BUILD_NUMBER" --template-url "https://s3.amazonaws.com/cf-templates-1bpde8xxropa1-us-east-1/website.template" --tags '[{"Key":"cf","Value":"true"},{"Key":"app","Value":"jenkins"}]' --parameters '[{"amiID":"$AMI_ID"}]'
+aws cloudformation create-stack --stack-name "website$BUILD_NUMBER" --template-url "https://s3.amazonaws.com/cf-templates-1bpde8xxropa1-us-east-1/website.template" --tags '[{"Key":"cf","Value":"true"},{"Key":"app","Value":"jenkins"}]' --parameters [\{\"ParameterKey\"\:\"amiID\"\,\"ParameterValue\"\:\"$AMI_ID\"\}]
